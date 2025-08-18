c7f9e7d1919397fba75b0504ca5c3c71ff9f6990
Ruchir Shah <@ruchirlives>
2025-08-18 17:47:20 +0100

    refactor: remove clearLayers call from LoadModal

--- src/components/ModalLoad.js
+++ src/components/ModalLoad.js
@@ -8,7 +8,7 @@ Modal.setAppElement("#app");
 const LoadModal = ({ isOpen, setIsOpen, setRowData, gridApiRef, setCurrentContainer, merge }) => {
     const [list, setList] = useState([]);
 
-    const { setLastLoadedFile, clearLayers } = useAppContext();
+    const { setLastLoadedFile } = useAppContext();
 
     const closeModal = () => {
         setIsOpen(false);
@@ -25,7 +25,6 @@ const LoadModal = ({ isOpen, setIsOpen, setRowData, gridApiRef, setCurrentContai
             await loadContainers(item);
         }
         setLastLoadedFile(item);
-        clearLayers();
 
         const channel = new BroadcastChannel('requestRefreshChannel');
         channel.postMessage({ type: "reload" });
