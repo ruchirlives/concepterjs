65c2bbb62dd8d8aecb2b7bd152cef5abcdbd2d09
Ruchir Shah <@ruchirlives>
2025-09-16 19:38:44 +0100

    feat: update API_URLS to include specific mongo and firebase endpoints

--- src/hooks/globalconfig.js
+++ src/hooks/globalconfig.js
@@ -1,5 +1,6 @@
 const API_URLS = {
-    cloudhost: "https://concepter-web-847876050649.europe-west2.run.app",
+    mongo: "https://concepter-web-847876050649.europe-west2.run.app",
+    firebase: "https://concepter-firebase-847876050649.europe-west2.run.app",
     localhost: "http://localhost:8080",
     // Add more servers as needed
 };
