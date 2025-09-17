0e3681b890e8f34dfe638b14e2957c13a780b329
Ruchir Shah <@ruchirlives>
2025-09-17 18:04:58 +0100

    feat: update API_URLS to use environment variable for dynamic configuration

--- src/hooks/globalconfig.js
+++ src/hooks/globalconfig.js
@@ -1,8 +1,3 @@
-const API_URLS = {
-    mongo: "https://concepter-web-847876050649.europe-west2.run.app",
-    firebase: "https://concepter-firebase-847876050649.europe-west2.run.app",
-    localhost: "http://localhost:8080",
-    // Add more servers as needed
-};
+const API_URLS = JSON.parse(process.env.REACT_APP_API_URLS);
 
 export default API_URLS;
